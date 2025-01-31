6e61903abd499952abf96a77a3b1b65ae21d80ae
Gauri Singhal <122613550+Gauri1singhal@users.noreply.github.com>
2025-01-31 09:40:30 +0530

    Create 2. Two Sum.cpp

---
+++ Phase 1/Day 02/2. Two Sum.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        // vector<int>ans;
+        // for(int i=0; i<nums.size(); i++){
+        //     for(int j=i+1; j<nums.size(); j++){
+        //         if(nums[i]+nums[j]==target){
+        //             ans.push_back(i);
+        //             ans.push_back(j);
+        //         }
+        //     }
+        // }
+        // return ans;
+        
+        unordered_map<int, int>mp;
+        for(int i=0; i<nums.size(); i++){
+            mp[nums[i]]=i;
+        }
+        for(int i=0; i<nums.size(); i++){
+            int tar= target-nums[i];
+            if(mp.find(tar)!=mp.end() && mp[tar]!=i){
+                return{i, mp[tar]};
+            }
+        }
+        return {-1};
+    }
+};
